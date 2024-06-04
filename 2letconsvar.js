b9484f1e8a09cfc2860ca5446da2d8f1276db2d6
Aditya Kumar Mishra <104449755+adi956@users.noreply.github.com>
2024-06-04 23:00:24 +0530

    Add files via upload

---
+++ 2letconsvar.js
@@ -0,0 +1,6 @@
+const empid = 12345
+let empname = "Aditya"
+var empemail = "aditya@gmail.com"
+empstate = "Uttar Pradesh"
+//prefer not to use var because it has issue in block scope and functionl scope
+console.table([empemail,empid,empname,empstate])
---
+++ 3datatypes.js
@@ -0,0 +1,2 @@
+//"Use Strict";  it tells the code is written in the latest ver of javascript
+//alert("Aditya!") only runs on browser
